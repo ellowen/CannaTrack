5eceaef8567c115692d207c82991374c72fe3e29
Ellowen <lrubio@sacoa.com>
2026-04-27 08:35:35 -0300

    fix: habilitar unstable_transformImportMeta en babel-preset-expo para web

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -4,7 +4,7 @@ module.exports = function (api) {
   // which can't see expo-router installed locally in mobile/node_modules. Add it explicitly.
   const { expoRouterBabelPlugin } = require('babel-preset-expo/build/expo-router-plugin')
   return {
-    presets: ['babel-preset-expo'],
+    presets: [['babel-preset-expo', { unstable_transformImportMeta: true }]],
     plugins: [
       expoRouterBabelPlugin,
       ['module-resolver', {
